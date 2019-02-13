6e399ee6f7b2fa43b7132dc5acace85dbc454d87
Nei <nei.neto@teialabs.com>
2019-02-13 15:14:32 -0200

    Sort imports

--- train.py
+++ train.py
@@ -1,27 +1,30 @@
 from __future__ import print_function
 
-import sys
-import os
 import argparse
+import os
+import sys
+from datetime import datetime
+
 import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+from torch.autograd import Variable
+
+import cv2
+from lib.ssds_train import train_model
+from lib.utils.config_parse import cfg_from_file
+
 if '/data/software/opencv-3.4.0/lib/python2.7/dist-packages' in sys.path:
     sys.path.remove('/data/software/opencv-3.4.0/lib/python2.7/dist-packages')
 if '/data/software/opencv-3.3.1/lib/python2.7/dist-packages' in sys.path:
     sys.path.remove('/data/software/opencv-3.3.1/lib/python2.7/dist-packages')
-import cv2
-from datetime import datetime
 
-import torch
-import torch.nn as nn
-import torch.backends.cudnn as cudnn
-from torch.autograd import Variable
 
-from lib.utils.config_parse import cfg_from_file
-from lib.ssds_train import train_model
 
 def parse_args():
     """
-    Parse input arguments
+    Parse input arguments.
     """
     parser = argparse.ArgumentParser(description='Train a ssds.pytorch network')
     parser.add_argument('--cfg', dest='config_file',
